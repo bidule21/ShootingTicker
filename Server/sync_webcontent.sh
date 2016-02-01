935ccfe83b9e9c164a99865a6ed3b11851325228
janxb <github@janbrodda.de>
2016-02-01 19:30:45 +0100

    added helper script (syncs target resources back to src folder)

---
+++ Server/sync_webcontent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/bin/cp -r "target/server-1.0-SNAPSHOT/scripts" "src/main/webapp"
+/bin/cp -r "target/server-1.0-SNAPSHOT/stylesheets" "src/main/webapp"
+/bin/cp -r "target/server-1.0-SNAPSHOT/images" "src/main/webapp"
+/bin/cp -r "target/server-1.0-SNAPSHOT/"*.jsp "src/main/webapp"
+
+sleep 3

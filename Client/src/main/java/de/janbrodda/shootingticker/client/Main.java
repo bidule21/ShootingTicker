ba202d86c21ccea986fbe3082e739ab5836b41c2
janxb <github@janbrodda.de>
2016-02-02 15:45:18 +0100

    implemented WebRequest class and updated corresponding sample code

--- Client/src/main/java/de/janbrodda/shootingticker/client/Main.java
+++ Client/src/main/java/de/janbrodda/shootingticker/client/Main.java
@@ -2,6 +2,8 @@ package de.janbrodda.shootingticker.client;
 
 import com.google.gson.Gson;
 
+import de.janbrodda.shootingticker.client.api.API;
+import de.janbrodda.shootingticker.client.api.WebRequest;
 import de.janbrodda.shootingticker.client.data.Competition;
 import de.janbrodda.shootingticker.client.files.FileWatcher;
 import de.janbrodda.shootingticker.client.files.shooters.ShooterFileLister;
@@ -15,7 +17,30 @@ public class Main {
 		final String filePath = "C:/tmp";
 
 		Settings s = Settings.get();
-		s.setApiKey("test");
+
+		API a = API.get();
+
+		s.useProxy = false;
+		s.proxyHost = "proxy";
+		s.proxyPort = 8080;
+		s.apiKey = "fdhdfdrshger";
+		s.save();
+
+		System.out.println("Starting");
+		//WebRequest r = new WebRequest(WebRequest.Method.POST, "https://shootingticker.appspot.com/api/put");
+		WebRequest r = new WebRequest(WebRequest.Method.POST, "http://localhost:8080/api/put");
+
+		Competition c = new Competition();
+		c.numShots = 40;
+		c.remainingSeconds = 0;
+		c.name = "öäüÜÄÖß";
+		c.id = 5629499534213120L;
+
+		r.parameters.put("key", s.apiKey);
+		r.parameters.put("competition", c.toJson());
+
+		System.out.println(r.load());
+		System.out.println("Finished");
 
 		if (1 + 1 == 2) {
 			return;
---
+++ Client/src/main/java/de/janbrodda/shootingticker/client/api/WebRequest.java
@@ -0,0 +1,152 @@
+package de.janbrodda.shootingticker.client.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+
+import de.janbrodda.shootingticker.client.Settings;
+
+public class WebRequest {
+	public static enum Method {
+		GET,
+		POST
+	}
+
+	private static HttpClient CLIENT;
+	private static String USERAGENT = "hu";
+	private static int CONNECTION_TIMEOUT = 10 * 1000;
+	private static Settings settings = Settings.get();
+
+	public Method method;
+	public String url;
+	public Map<String, String> parameters = new HashMap<>();
+
+	public WebRequest(Method method, String url) {
+		if (CLIENT == null) {
+			prepareHttpClient();
+		}
+
+		this.method = method;
+		this.url = url;
+	}
+
+	public String load() {
+		HttpResponse response = null;
+
+		switch (method) {
+		case GET:
+			response = loadGet();
+			break;
+		case POST:
+			response = loadPost();
+		}
+
+		if (response == null) {
+			return null;
+		}
+
+		switch (response.getStatusLine().getStatusCode()) {
+		case 200:
+
+			try {
+				BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+				StringBuffer result = new StringBuffer();
+				String line = "";
+				while ((line = rd.readLine()) != null) {
+					result.append(line);
+				}
+
+				return result.toString();
+
+			} catch (UnsupportedOperationException | IOException ex) {
+				ex.printStackTrace();
+				return null;
+			}
+
+		default:
+			System.err.println(response.toString());
+			return null;
+		}
+	}
+
+	private void prepareHttpClient() {
+		RequestConfig.Builder requestBuilder = RequestConfig.custom()
+				.setConnectTimeout(CONNECTION_TIMEOUT)
+				.setConnectionRequestTimeout(CONNECTION_TIMEOUT)
+				.setSocketTimeout(CONNECTION_TIMEOUT)
+				.setContentCompressionEnabled(false);
+
+		CredentialsProvider proxyCredentials = new BasicCredentialsProvider();
+
+		if (settings.useProxy) {
+			requestBuilder.setProxy(new HttpHost(settings.proxyHost, settings.proxyPort));
+			if (settings.proxyPass != null && settings.proxyUser != null) {
+				UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(settings.proxyUser, settings.proxyPass);
+				proxyCredentials.setCredentials(AuthScope.ANY, credentials);
+			}
+		}
+
+		HttpClientBuilder builder = HttpClientBuilder.create();
+		builder.setDefaultRequestConfig(requestBuilder.build());
+
+		if (settings.useProxy) {
+			CLIENT = builder.setDefaultCredentialsProvider(proxyCredentials).build();
+		} else {
+			CLIENT = builder.build();
+		}
+	}
+
+	private HttpResponse loadGet() {
+		HttpGet request = new HttpGet(url);
+		try {
+			return CLIENT.execute(request);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
+	private HttpResponse loadPost() {
+		HttpPost request = new HttpPost(url);
+
+		ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+
+		for (Entry<String, String> entry : parameters.entrySet()) {
+			String key = entry.getKey();
+			String value = entry.getValue();
+
+			postParameters.add(new BasicNameValuePair(key, value));
+		}
+
+		try {
+			// This bit is EXTREMLY important for transmitting umlauts (äöüß)!
+			UrlEncodedFormEntity entity = new UrlEncodedFormEntity(postParameters);
+			entity.setContentType("application/x-www-form-urlencoded; charset=iso-8859-1");
+			request.setEntity(entity);
+
+			return CLIENT.execute(request);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+}

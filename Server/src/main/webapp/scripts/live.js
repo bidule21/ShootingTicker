8558ff1bf19b7bb810a1685e7576d9713c53db82
janxb <github@janbrodda.de>
2016-02-03 12:35:55 +0100

    added more settings and implemented ring numbers

--- Server/src/main/webapp/scripts/live.js
+++ Server/src/main/webapp/scripts/live.js
@@ -1,36 +1,72 @@
 var Target = {
 	settings : {
+		// how often should we poll for new Data
 		refreshInterval : 5000,
+		// how many shots will be displayed on the target
 		numShotsPerTarget : 5,
+		// min time to block page while initial loading
 		minLoadingTime : 1000,
+		// color of the leading shooter
 		winnerTargetColor : 'yellow',
+		// display all shots if shooter is finished
+		showFinishedTargets : true,
 
-		innerRingRadius : 10,
-		ringRadius : 50,
+		// border around a single ring
 		ringBorder : 5,
+		// radius of the inner ring (10)
+		innerRingRadius : 5,
+		// radius of every other ring (1-9)
+		ringRadius : 50,
+		// radius of the shot (should be same as ringRadius)
+		shotRadius : 50,
 
+		// color of the outer rings
 		lowRingColor : 'white',
-		highRingColor : 'green',
-		highRingNumber : 4,
+		// color of the inner rings
+		medRingColor : 'green',
+		// fill color of the innermost ring
+		highRingColor : 'black',
+		// text color if the outer rings
+		lowRingTextColor : 'black',
+		// text color of the inner rings
+		medRingTextColor : 'white',
+		// inner rings start number
+		medRingNumber : 4,
+		// innermost ring start number
+		highRingNumber : 10,
+		// font size of ring number text
+		ringNumberFontSize : 20,
 
+		// color of an inactive shot
 		inactiveShotColor : '#333',
+		// color of a shot with low value
 		lowShotColor : 'blue',
+		// color of a shot with medium value
 		medShotColor : 'yellow',
+		// color of a shot with high value
 		highShotColor : 'red',
+		// everything higher is a medium shot
 		medShotResult : 9.0,
+		// everything higher is a high shot
 		highShotResult : 10.0,
 
+		// font size of the shot number
 		shotNumberFontSize : 50,
-		shotnumberForegroundColor : 'black',
-		shotNumberBackgroundColor : '#ccc',
+		
+		// color of the current shot
+		shotnumberActiveColor : 'black',
+		// color of shots in the background
+		shotNumberInactiveColor : '#ccc',
 
-		originalShotScale : 4.2,
+		// value to divide the original shot coordinates for scaling
+		originalShotScale : 4.9,
+		// ssize of the canvas to calculate coordinates with
 		canvasSize : 1000,
 
+		// list of possible zoomfactors. Have to be same length as -zoomFactorLimits-
 		zoomFactors : [ 1, 1.9, 2.2, 2.5, 2.8, 3.3, 4.2, 5, 6 ],
-		zoomFactorLimits : [ 900, 800, 700, 600, 450, 300, 150, 50, 0 ],
-
-		shotRadius : 50
+		// list of zoom factor limits. Values have to match factors defined in -zoomFactors-
+		zoomFactorLimits : [ 900, 800, 700, 600, 450, 300, 150, 50, 0 ]
 	},
 
 	prepare : function(canvas) {
@@ -51,46 +87,43 @@ var Target = {
 		var zoomFactor = canvas.attr("data-zoom") != null ? canvas
 				.attr("data-zoom") : 1;
 
-		var color;
+		var shotColor;
 		if (!lastShot) {
-			color = this.settings.inactiveShotColor;
+			shotColor = this.settings.inactiveShotColor;
 		} else if (shot.result < this.settings.medShotResult) {
-			color = this.settings.lowShotColor;
+			shotColor = this.settings.lowShotColor;
 		} else if (shot.result < this.settings.highShotResult) {
-			color = this.settings.medShotColor;
+			shotColor = this.settings.medShotColor;
 		} else {
-			color = this.settings.highShotColor;
+			shotColor = this.settings.highShotColor;
 		}
+		
+		shot.x = shot.x / this.settings.originalShotScale * zoomFactor;
+		shot.y = shot.y / this.settings.originalShotScale * zoomFactor;
 
 		canvas.drawArc({
-			fillStyle : color,
-			x : this.settings.canvasSize / 2 + shot.x
-					/ this.settings.originalShotScale * zoomFactor,
-			y : this.settings.canvasSize / 2 + shot.y
-					/ this.settings.originalShotScale * zoomFactor,
+			fillStyle : shotColor,
+			x : this.settings.canvasSize / 2 + shot.x,
+			y : this.settings.canvasSize / 2 + shot.y,
 			radius : this.settings.shotRadius,
 			strokeStyle : 'black',
 			strokeWidth : this.settings.ringBorder / zoomFactor,
 			scale : zoomFactor
 		});
 
-		var textColor = lastShot ? this.settings.shotnumberForegroundColor
-				: this.settings.shotNumberBackgroundColor;
+		var textColor = lastShot ? this.settings.shotnumberActiveColor
+				: this.settings.shotNumberInactiveColor;
 
 		canvas.drawText({
 			fillStyle : textColor,
-			strokeStyle : 'black',
-			strokeWidth : 1,
-			x : this.settings.canvasSize / 2 + shot.x
-					/ this.settings.originalShotScale * zoomFactor,
-			y : this.settings.canvasSize / 2 + shot.y
-					/ this.settings.originalShotScale * zoomFactor,
+			strokeWidth : 0,
+			x : this.settings.canvasSize / 2 + shot.x,
+			y : this.settings.canvasSize / 2 + shot.y,
 			fontSize : this.settings.shotNumberFontSize * zoomFactor,
-			fontFamily : 'Arial',
 			text : shot.num
 		});
 	},
-
+	
 	drawBackground : function(canvas) {
 		canvas.drawRect({
 			fillStyle : 'darkblue',
@@ -104,24 +137,64 @@ var Target = {
 	drawRing : function(canvas, number) {
 		var zoomFactor = canvas.attr("data-zoom") != null ? canvas
 				.attr("data-zoom") : 1;
-		var color;
-		if (number < this.settings.highRingNumber) {
-			color = this.settings.lowRingColor;
+				
+		var ringColor;
+		var textColor;
+		
+		if (number < this.settings.medRingNumber) {
+			ringColor = this.settings.lowRingColor;
+			textColor = this.settings.lowRingTextColor;
+		} else if (number < this.settings.highRingNumber){
+			ringColor = this.settings.medRingColor;
+			textColor = this.settings.medRingTextColor;
 		} else {
-			color = this.settings.highRingColor;
+			ringColor = this.settings.highRingColor;
 		}
 
 		var radius = number == 10 ? this.settings.innerRingRadius
 				: this.settings.ringRadius * (10 - number);
+		
+		var ringWidth = number == 10 ? radius - this.settings.innerRingRadius
+				: radius - this.settings.ringRadius * (10 - number-zoomFactor / 2);
 
 		canvas.drawArc({
-			fillStyle : color,
-			x : this.settings.canvasSize / 2,
-			y : this.settings.canvasSize / 2,
+			fillStyle : ringColor,
+			x : (this.settings.canvasSize / 2),
+			y : (this.settings.canvasSize / 2),
 			radius : radius,
 			strokeStyle : 'black',
 			strokeWidth : this.settings.ringBorder / zoomFactor,
 			scale : zoomFactor
+		});
+		
+		if (number < 9){
+			var base = this.settings.canvasSize / 2;
+			var plus = this.settings.canvasSize / 2 + radius * zoomFactor - ringWidth - 5;
+			var minus = this.settings.canvasSize / 2 - radius * zoomFactor + ringWidth + 5;
+			
+			this.drawRingNumber(canvas, number, textColor, base, plus);
+			this.drawRingNumber(canvas, number, textColor, plus, base);
+			this.drawRingNumber(canvas, number, textColor, base, minus);
+			this.drawRingNumber(canvas, number, textColor, minus, base);
+		}
+	},
+	
+	drawRingNumber : function(canvas, number, textColor, x, y){
+		var zoomFactor = canvas.attr("data-zoom") != null ? canvas
+				.attr("data-zoom") : 1;
+		var radius = number == 10 ? this.settings.innerRingRadius
+				: this.settings.ringRadius * (10 - number);
+		var ringWidth = number == 10 ? radius - this.settings.innerRingRadius
+				: radius - this.settings.ringRadius * (10 - number-zoomFactor / 2);
+		
+		canvas.drawText({
+			fillStyle : textColor,
+			strokeWidth : 0,
+			y : y,
+			x : x,
+			fontSize : this.settings.ringNumberFontSize * zoomFactor,
+			fontFamily : 'Arial',
+			text : number
 		});
 	},
 
@@ -209,9 +282,13 @@ function AppViewModel() {
 	self.drawShooter = function(shooter) {
 		var canvas = $("#target_" + shooter.laneNumber);
 
-		var shotList = shooter.shots.length >= self.competition().numShots ? shooter.shots
-				: shooter.shots.slice(0 - Target.settings.numShotsPerTarget);
-
+		var shotList;
+		if (shooter.shots.length >= self.competition().numShots && Target.settings.showFinishedTargets){
+			shotList =shooter.shots;
+		} else {
+			shotList = shooter.shots.slice(0 - Target.settings.numShotsPerTarget);
+		}	
+		
 		var maxCoordinate = 0;
 		shotList.forEach(function(shot) {
 			var posX = Math.abs(shot.x);

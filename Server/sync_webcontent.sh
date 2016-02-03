b0581dd852d9b89c874f78c7ec46727c67a16322
janxb <github@janbrodda.de>
2016-02-03 12:34:09 +0100

    switch back to /bin/cp, because rsync is not working properly on cygwin

--- Server/sync_webcontent.sh
+++ Server/sync_webcontent.sh
@@ -2,10 +2,10 @@
 
 while true; do
 
-rsync -a --delete 	target/server-1.0-SNAPSHOT/images 		src/main/webapp
-rsync -a --delete 	target/server-1.0-SNAPSHOT/stylesheets	src/main/webapp
-rsync -a --delete 	target/server-1.0-SNAPSHOT/scripts 		src/main/webapp
-/bin/cp -r 			target/server-1.0-SNAPSHOT/*.jsp		src/main/webapp
+/bin/cp -r	target/server-1.0-SNAPSHOT/images 		src/main/webapp
+/bin/cp -r	target/server-1.0-SNAPSHOT/stylesheets	src/main/webapp
+/bin/cp -r	target/server-1.0-SNAPSHOT/scripts 		src/main/webapp
+/bin/cp -r	target/server-1.0-SNAPSHOT/*.jsp		src/main/webapp
 
 sleep 10
 

ef439969d04fb8786d25fbfc251cca26665cb285
janxb <github@janbrodda.de>
2016-02-10 14:10:43 +0100

    load local competitions in uploadPanel and small formatting fixes

--- Client/src/main/java/de/janbrodda/shootingticker/client/Main.java
+++ Client/src/main/java/de/janbrodda/shootingticker/client/Main.java
@@ -1,10 +1,22 @@
 package de.janbrodda.shootingticker.client;
 
 import de.janbrodda.shootingticker.client.gui.GUI;
+import de.janbrodda.shootingticker.client.settings.Settings;
 
 public class Main {
 
     public static void main(String[] args) {
+		Settings s = Settings.get();
+		s.apiUrl = "http://localhost:8080";
+		s.apiKey = "fdhdfdrshger";
+		s.competitionBasePath = "C:/tmp";
+		
         GUI.main(args);
+		
+		/**
+		 * Notes:
+		 * - upload GUI should show competition teams when selecting from localCompetitionsDropdown
+		 *
+		 */
     }
 }
